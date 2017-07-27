a9b0ec1f1df40f1fc7415dd9ba4d718b3beb3ae4
Jamie Aronson <jamie.aronson@roqlogic.com>
2017-07-26 17:20:46 -0700

    initial commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+	apiGateway: {
+		URL: 'https://ktzg159n7e.execute-api.us-west-2.amazonaws.com/prod',
+		REGION: 'us-west-2',
+	},
+	cognito: {
+		USER_POOL_ID: 'us-west-2_YnuunK6Vt',
+		APP_CLIENT_ID: '9ild1ng6jftf52covdrgtkbns',
+		IDENTITY_POOL_ID: 'us-west-2:e79f4284-24d9-454c-a13c-fc4f189719f6',
+		REGION: 'us-west-2',
+	},
+	s3: {
+		BUCKET: 'mc-notes-jamie',
+	},
+	MAX_ATTACHMENT_SIZE: 5000000,
+};
